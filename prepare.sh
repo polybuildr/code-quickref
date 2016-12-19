e9050d9e86a2e8f91d7bbd7403e1655ce90c089a
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2016-12-19 23:58:52 +0530

    Fix bug in prepare script when multiple visualizer html files exist
    grep also prints the file's name, which messes up the rsync command. -h
    removes the file name from the output.

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,4 @@ for i in pages/*.md; do echo -n "Building $i... "; node output-page.js $i; echo 
 echo -n "Copying visualizer... "
 rm -rf public/vis; cp vis -r public/vis;
 echo "Done."
-for i in $(grep -oE 'bower_components/[^"]*' vis/*.html); do echo $i; rsync -R $i public/; done
+for i in $(grep -ohE 'bower_components/[^"]*' vis/*.html); do echo $i; rsync -R $i public/; done
